18ebca4db3a8eeb2ecb3c69d327bc4e8c6362d40
jt-z <jianting112358@gmail.com>
2026-01-29 23:58:39 +0800

    single card bs 8 to 4

--- scripts/train_tiny_zero.sh
+++ scripts/train_tiny_zero.sh
@@ -11,32 +11,34 @@ export RAY_memory_monitor_refresh_ms=0
 # [Batch Size 策略]
 # Global Micro = 8  (单卡 Batch = 1)
 # Global Mini = 128
+export single_gpu_size=4
 
 python3 -m verl.trainer.main_ppo \
     data.train_files=$DATA_DIR/train.parquet \
     data.val_files=$DATA_DIR/test.parquet \
     data.train_batch_size=128 \
-    data.val_batch_size=1312 \
+    data.val_batch_size=128 \
     data.max_prompt_length=256 \
-    data.max_response_length=768 \
+    data.max_response_length=1024 \
     actor_rollout_ref.model.path=$BASE_MODEL \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
     actor_rollout_ref.actor.use_dynamic_bsz=True \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.actor.ppo_mini_batch_size=128 \
-    actor_rollout_ref.actor.ppo_micro_batch_size=8 \
-    actor_rollout_ref.rollout.log_prob_micro_batch_size=8 \
+    actor_rollout_ref.actor.ppo_micro_batch_size=4\
+    actor_rollout_ref.rollout.log_prob_micro_batch_size=4 \
     actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.2 \
-    actor_rollout_ref.ref.log_prob_micro_batch_size=8 \
+    actor_rollout_ref.ref.log_prob_micro_batch_size=4 \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
     actor_rollout_ref.actor.fsdp_config.param_offload=False \
     actor_rollout_ref.ref.fsdp_config.param_offload=False \
     critic.optim.lr=1e-5 \
     critic.model.path=$BASE_MODEL \
-    critic.ppo_micro_batch_size=8 \
+    critic.ppo_micro_batch_size=4 \
+    critic.ppo_mini_batch_size=128 \
     critic.model.enable_gradient_checkpointing=True \
     critic.model.fsdp_config.optimizer_offload=False \
     critic.model.fsdp_config.param_offload=False \
